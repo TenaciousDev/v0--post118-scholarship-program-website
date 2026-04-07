55701546a08e8dd851f7c94bf415033b8352f199
adam metcalf <adam.s.metcalf@outlook.com>
2026-04-07 14:36:53 -0400

    trying again Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   basePath: '/v0--post118-scholarship-program-website', // Replace with your GitHub repo name
   assetPrefix: '/v0--post118-scholarship-program-website/', 
   images: {
-    unoptimized: true,        // Required for GitHub Pages
+    unoptimized: true,        // Required for GitHub Pages TEST
   },
   trailingSlash: true,        // Optional but recommended
 };
